85791bb92d1b6a27ce8ce47dfd1162a12254c898
YannickLeRoux <rasyann@hotmail.com>
2019-07-22 09:00:20 -0700

    stats

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,22 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numberCollection = new NumbersCollection([1, 89, 4, 7, 6, 8, 9]);
-// const sorter = new Sorter(numberCollection);
-// sorter.sort();
+// numberCollection.sort();
 // console.log(numberCollection.data);
 
-const charactersCollection = new CharactersCollection('jgoajsgsHH');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection('jgoajsgsHH');
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(100);
+linkedList.add(-5);
+linkedList.add(4);
+linkedList.add(-30);
+
+linkedList.sort();
+linkedList.print();
